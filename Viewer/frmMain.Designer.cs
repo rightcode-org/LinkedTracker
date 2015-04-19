3d5bf8fe8931422bc373bc6b678ebc11408dff3a
rightcode-org <rightcode.org@gmail.com>
2015-04-19 17:53:24 +0100

    LinkedTrackerView added
    LinkedTrackerView C# project is added. This views images sent by
    LinkedTracker robot via Serial port.

---
+++ Viewer/frmMain.Designer.cs
@@ -0,0 +1,369 @@
+﻿/*
+	Copyright(c) 2015, [kaisar] @ rightcode.org
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met :
+
+	*Redistributions of source code must retain the above copyright notice, this
+	list of conditions and the following disclaimer.
+
+	* Redistributions in binary form must reproduce the above copyright notice,
+	this list of conditions and the following disclaimer in the documentation
+	and / or other materials provided with the distribution.
+
+	* Neither the name of LinkedTracker nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+namespace LinkedTrackerView
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.serial_ = new System.IO.Ports.SerialPort(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.chkZoom = new System.Windows.Forms.CheckBox();
+            this.btnTrack = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pic8 = new System.Windows.Forms.PictureBox();
+            this.btnLearn = new System.Windows.Forms.Button();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic1.Location = new System.Drawing.Point(12, 63);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(160, 120);
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic2.Location = new System.Drawing.Point(12, 204);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(160, 120);
+            this.pic2.TabIndex = 0;
+            this.pic2.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic3.Location = new System.Drawing.Point(178, 204);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(160, 120);
+            this.pic3.TabIndex = 0;
+            this.pic3.TabStop = false;
+            // 
+            // pic4
+            // 
+            this.pic4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic4.Location = new System.Drawing.Point(344, 204);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(160, 120);
+            this.pic4.TabIndex = 0;
+            this.pic4.TabStop = false;
+            // 
+            // pic6
+            // 
+            this.pic6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic6.Location = new System.Drawing.Point(178, 63);
+            this.pic6.Name = "pic6";
+            this.pic6.Size = new System.Drawing.Size(160, 120);
+            this.pic6.TabIndex = 0;
+            this.pic6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(178, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Filtered:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Overlay:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(50, 9);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(54, 20);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "COM6";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(110, 8);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(82, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // serial_
+            // 
+            this.serial_.BaudRate = 460800;
+            this.serial_.DtrEnable = true;
+            this.serial_.PortName = "COM6";
+            this.serial_.ReadBufferSize = 16000;
+            this.serial_.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serial_DataReceived);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(57, 333);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(12, 333);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 13);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Time";
+            // 
+            // chkZoom
+            // 
+            this.chkZoom.AutoSize = true;
+            this.chkZoom.Location = new System.Drawing.Point(214, 11);
+            this.chkZoom.Name = "chkZoom";
+            this.chkZoom.Size = new System.Drawing.Size(67, 17);
+            this.chkZoom.TabIndex = 5;
+            this.chkZoom.Text = "Zoom 2x";
+            this.chkZoom.UseVisualStyleBackColor = true;
+            this.chkZoom.CheckedChanged += new System.EventHandler(this.chkZoom_CheckedChanged);
+            // 
+            // btnTrack
+            // 
+            this.btnTrack.Location = new System.Drawing.Point(510, 63);
+            this.btnTrack.Name = "btnTrack";
+            this.btnTrack.Size = new System.Drawing.Size(77, 25);
+            this.btnTrack.TabIndex = 6;
+            this.btnTrack.Text = "Track";
+            this.btnTrack.UseVisualStyleBackColor = true;
+            this.btnTrack.Click += new System.EventHandler(this.btnCmdBackg_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 188);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Background:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(178, 188);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(32, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Gray:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(341, 188);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Diff:";
+            // 
+            // pic8
+            // 
+            this.pic8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic8.Location = new System.Drawing.Point(344, 63);
+            this.pic8.Name = "pic8";
+            this.pic8.Size = new System.Drawing.Size(160, 120);
+            this.pic8.TabIndex = 0;
+            this.pic8.TabStop = false;
+            // 
+            // btnLearn
+            // 
+            this.btnLearn.Location = new System.Drawing.Point(510, 204);
+            this.btnLearn.Name = "btnLearn";
+            this.btnLearn.Size = new System.Drawing.Size(77, 25);
+            this.btnLearn.TabIndex = 7;
+            this.btnLearn.Text = "Learn";
+            this.btnLearn.UseVisualStyleBackColor = true;
+            this.btnLearn.Click += new System.EventHandler(this.btnLearn_Click);
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(344, 330);
+            this.txtFilter.Multiline = true;
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(160, 19);
+            this.txtFilter.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Filter:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 359);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.btnLearn);
+            this.Controls.Add(this.btnTrack);
+            this.Controls.Add(this.chkZoom);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic8);
+            this.Controls.Add(this.pic6);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LinkedView";
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.IO.Ports.SerialPort serial_;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.CheckBox chkZoom;
+        private System.Windows.Forms.Button btnTrack;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox pic8;
+        private System.Windows.Forms.Button btnLearn;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Viewer/frmMain.cs
@@ -0,0 +1,282 @@
+﻿/*
+	Copyright(c) 2015, [kaisar] @ rightcode.org
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met :
+
+	*Redistributions of source code must retain the above copyright notice, this
+	list of conditions and the following disclaimer.
+
+	* Redistributions in binary form must reproduce the above copyright notice,
+	this list of conditions and the following disclaimer in the documentation
+	and / or other materials provided with the distribution.
+
+	* Neither the name of LinkedTracker nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkedTrackerView
+{
+    public partial class frmMain : Form
+    {
+        const int BUFF_SIZE = 16 * 1024;
+        const int PIC_W = 80;
+        const int PIC_H = 60;
+
+        byte[] buff_ = new byte[BUFF_SIZE];
+        bool zoom2x = false;
+
+        enum PicType
+        {
+            CamRaw, Backg, Gray, Xor, Filtered, NoiseReduced, Blobs
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (serial_.IsOpen)
+            {
+                serial_.Close();
+                lblStatus.Text = "Disconnected";
+                btnConnect.Text = "Connect";
+            }
+            else
+            {
+                serial_.PortName = txtPort.Text;
+                try
+                {
+                    serial_.Open();
+                    lblStatus.Text = "Connected to " + txtPort.Text;
+                    btnConnect.Text = "Disconnect";
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text = "Could not connect to " + txtPort.Text;
+                }
+            }//else
+        }
+
+        private void ReadFilter()
+        {
+            //wait for data
+            while (serial_.BytesToRead < 6)
+                System.Threading.Thread.Sleep(20);
+
+            serial_.Read(buff_, 0, 6);
+
+            txtFilter.Text = (int)buff_[0] + ", " + (int)buff_[1] + ", " + (int)buff_[2] + " - ";
+            txtFilter.Text += (int)buff_[3] + ", " + (int)buff_[4] + ", " + (int)buff_[5];
+        }
+
+        private void ReadPic(PicType type)
+        {
+            int size = PIC_H * PIC_W;
+
+            if (type == PicType.CamRaw)
+                size *= 2;
+
+            //wait for data
+            while (serial_.BytesToRead < size)
+                System.Threading.Thread.Sleep(20);
+
+            serial_.Read(buff_, 0, size);
+
+            int inc = 1;
+            int picW = PIC_W, picH = PIC_H;
+
+            if (zoom2x)
+            {
+                inc++;
+                picW *= 2;
+                picH *= 2;
+            }
+
+            Bitmap bmp = new Bitmap(picW, picH);
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.Clear(Color.White);
+
+            int c = 0;
+            if (type == PicType.CamRaw)
+            {
+                pic1.Image = bmp;
+
+                for (int i = 0; i < picH; i += inc)
+                    //for (int j = PIC_W; j >= 1; --j)
+                    for (int j = 0; j < picW; j += inc)
+                    {
+                        int pix = buff_[c++] << 8;    //high byte
+                        pix += buff_[c++];            //low byte
+
+                        byte b = (byte)(pix & 0x1F);  //lower 5 bits
+                        pix = pix >> 5;               //discard lower 5 bits
+                        byte g = (byte)(pix & 0x3F);  //lower 6 bits;
+                        g >>= 1;
+                        pix = pix >> 6;               //discard lower 6 bits
+                        byte r = (byte)(pix & 0x1F);  //rest 5 bits;
+                        
+                        Color col = Color.FromArgb(255 * r / 32, 255 * g / 32, 255 * b / 32);
+                        bmp.SetPixel(j, i, col);
+
+                        if (zoom2x)
+                        {
+                            bmp.SetPixel(j, i + 1, col);
+                            bmp.SetPixel(j + 1, i, col);
+                            bmp.SetPixel(j + 1, i + 1, col);
+                        }
+                    }//for
+                return;
+            }//if
+
+            byte blob_inc = 50;
+            byte last_pix = 0;
+
+            for (int i = 0; i < picH; i += inc)
+                //for (int j = PIC_W; j >= 1; --j)
+                for (int j = 0; j < picW; j += inc)
+                {
+                    byte pix = buff_[c++];
+
+                    if (pix != 0)
+                    {
+                        if (type == PicType.Xor || type == PicType.Filtered || type == PicType.NoiseReduced)
+                                pix = 255;
+                        else if (type == PicType.Blobs)
+                        {
+                            if (pix != last_pix)
+                            {
+                                pix += blob_inc;
+                                last_pix = pix;
+                                blob_inc += 50;
+                            }
+                        }//else if
+                    }//if pix
+
+                    Color col = Color.FromArgb(pix, pix, pix);
+                    bmp.SetPixel(j, i, col);
+
+                    if (zoom2x)
+                    {
+                        bmp.SetPixel(j, i + 1, col);
+                        bmp.SetPixel(j + 1, i, col);
+                        bmp.SetPixel(j + 1, i + 1, col);
+                    }
+                }//for j
+
+            if (type == PicType.Backg)
+                pic2.Image = bmp;
+            else if (type == PicType.Gray)
+                pic3.Image = bmp;
+            else if (type == PicType.Xor)
+                pic4.Image = bmp;
+            else if (type == PicType.NoiseReduced)
+                pic6.Image = bmp;
+
+        }
+
+        private void serial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            const int CMD_SIZE = 3;
+            while (true)
+            {
+                if (serial_.BytesToRead < CMD_SIZE)
+                    return;
+
+                serial_.Read(buff_, 0, CMD_SIZE);
+                string data = Encoding.Default.GetString(buff_, 0, CMD_SIZE);
+
+                string time = string.Format("{0:HH:mm:ss}", DateTime.Now);
+                lblStatus.Invoke((MethodInvoker)delegate { lblTime.Text = time; });
+
+                switch (data)
+                {
+                    case "SYN":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Sync Done"; });
+                        break;
+
+                    case "RAW":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Raw"; });
+                        break;
+
+                    case "CAM":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Cam"; ReadPic(PicType.CamRaw); });
+                        break;
+
+                    case "BAK":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Background"; ReadPic(PicType.Backg); });
+                        break;
+
+                    case "GRY":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Gray"; ReadPic(PicType.Gray); });
+                        break;
+
+                    case "XOR":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Xor"; ReadPic(PicType.Xor); });
+                        break;
+
+                    case "FIL":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Filtered"; ReadPic(PicType.Filtered); });
+                        break;
+
+                    case "NOI":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Filtered"; ReadPic(PicType.NoiseReduced); });
+                        break;
+
+                    case "BLO":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Blobs"; ReadPic(PicType.Blobs); });
+                        break;
+
+                    case "LRN":
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Learned"; ReadFilter(); });
+                        break;
+                    
+                    default:
+                        lblStatus.Invoke((MethodInvoker)delegate { lblStatus.Text = "Error";});
+                        serial_.DiscardInBuffer();
+                        break;
+
+                }//switch
+            }//while
+        }
+
+        private void chkZoom_CheckedChanged(object sender, EventArgs e)
+        {
+            zoom2x = chkZoom.Checked;
+        }
+
+        private void btnCmdBackg_Click(object sender, EventArgs e)
+        {
+            lblStatus.Invoke((MethodInvoker)delegate { serial_.Write("TRK"); });
+        }
+
+        private void btnLearn_Click(object sender, EventArgs e)
+        {
+            lblStatus.Invoke((MethodInvoker)delegate { serial_.Write("LRN"); });
+        }
+    }
+}
